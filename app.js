6b68e19638e49037496093d7b7ae0883a16435e8
Yulia <yulia.kovalova.741@gmail.com>
2021-03-26 02:26:56 +0200

    added adaptive 'follow on instagram' section with slider

--- app.js
+++ app.js
@@ -10,3 +10,35 @@ mobileMenu.addEventListener("click", function () {
         mainMenu.classList.remove("active-menu")
     }
 })
+
+// Add slider slick
+$(".slider-block").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerMode: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        }
+      ]
+})
